b0fb9ca1ade3d1be92d872b86170093945bbe1b9
04khofifah <2041720119@student.polinema.ac.id>
2022-02-26 10:17:47 +0700

    make controlller prodact

--- app/Http/Controllers/home3controller.php
+++ app/Http/Controllers/home3controller.php
@@ -1,13 +1,10 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
-
 class home3controller extends Controller
 {
     public function index(){
         return "Halaman Awal Website";
     }
-    //
+
 }
