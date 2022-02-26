b1d99d986f384dcb06336280f465d22408bb0bb9
04khofifah <2041720119@student.polinema.ac.id>
2022-02-26 10:27:15 +0700

    add fungsi pada file controllernya (ProductController.php)

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,5 +6,7 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+  public function product($id){
+        return '<a href="https://www.educastudio.com/category/'.$id.'">
+        https://www.educastudio.com/category/'.$id.'</a>';}  
 }
