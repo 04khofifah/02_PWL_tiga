81bee5806cf72b28a16d9135705b962e5fa54c9e
04khofifah <2041720119@student.polinema.ac.id>
2022-02-26 10:03:30 +0700

    membuat routing untuk halaman home

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
+use App\Http\Controllers\home3controller;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/home', [home3controller::class,'index']);
