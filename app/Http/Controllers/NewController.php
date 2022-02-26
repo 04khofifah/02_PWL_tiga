56ad26709d4a331fa962e39b33092890c2f030f0
04khofifah <2041720119@student.polinema.ac.id>
2022-02-26 17:33:03 +0700

    fungsi berparameter opsional pada file controllernya (NewsController.php)

--- app/Http/Controllers/NewController.php
+++ app/Http/Controllers/NewController.php
@@ -6,5 +6,10 @@ use Illuminate\Http\Request;
 
 class NewController extends Controller
 {
-    //
+        public function news($id = null){
+            if($id == null) return '<a href="https://www.educastudio.com/news">
+                https://www.educastudio.com/news</a>';
+            return '<a href="https://www.educastudio.com/news/'.$id.'">
+            https://www.educastudio.com/news/'.$id.'</a>';
+        }
 }
